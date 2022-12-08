f688363b554aef035deed337b50b170e9648d9d6
mohsinnadeem2016 <117082651+mohsinnadeem2016@users.noreply.github.com>
2022-12-08 05:19:06 -0800

    Add files via upload

---
+++ NumberPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NumberPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       {
+            {
+                public void NumberPattern()
+                {
+                    for (int i = 1; i < 10; i++)
+                    {
+                        for (int j = 10; j > i; j--)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int k = 1; k < i; k++)
+                        {
+                            Console.Write(k);
+                        }
+                        for (int l = i - 2; l >= 1; l--)
+                        {
+                            Console.Write(l);
+                        }
+                        Console.WriteLine("\n");
+                    }
+        }
+    }
+}
